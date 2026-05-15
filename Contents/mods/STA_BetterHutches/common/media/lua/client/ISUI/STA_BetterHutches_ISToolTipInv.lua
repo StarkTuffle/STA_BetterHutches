12d1e493bef5093ba3dd7e6ad30b7a1a68250782
StarkTruffle <astarkie-github@odraefys.dev>
2026-05-15 14:15:14 -0600

    Added additional guards for tooltip override

--- Contents/mods/STA_BetterHutches/common/media/lua/client/ISUI/STA_BetterHutches_ISToolTipInv.lua
+++ Contents/mods/STA_BetterHutches/common/media/lua/client/ISUI/STA_BetterHutches_ISToolTipInv.lua
@@ -3,7 +3,7 @@ local _old_ISToolTipInv_render = ISToolTipInv.render
 local Utils = require "STA_BetterHutches_Utils"
 
 function ISToolTipInv:render()
-    if self.item:hasTag(ItemTag.EGG) and self.item:isFertilized() and (self.tooltip:getCharacter():getPerkLevel(Perks.Husbandry) >= Utils.getSandboxInt("FertilizedEggLevel")) then
+    if self.item and self.item:isFood() and self.item:hasTag(ItemTag.EGG) and self.item:isFertilized() and self.tooltip and (self.tooltip:getCharacter():getPerkLevel(Perks.Husbandry) >= Utils.getSandboxInt("FertilizedEggLevel")) then
         local LINE_SPACING = self.tooltip:getLineSpacing()
         local extraH = LINE_SPACING
 
